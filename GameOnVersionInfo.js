ff2187422a8c89419867f8750c30ad4aa34dde4f
K Eric Mittler <eric.mittler@gmail.com>
2021-09-25 18:49:39 -0700

    GameOnVersionInfo

---
+++ GameOnVersionInfo.js
@@ -0,0 +1,17 @@
+
+class GameOnVersionInfo {
+  constructor({ node }) {
+    this.versions = { 'gameon-mcp23017': '50' }
+    this.node = node
+    this.node.on('input', (msg, send, done) => this.sendVersions({ msg, send, done }))
+    this.node.status({ fill: 'black', shape: 'dot', text: JSON.stringify(this.versions) })
+  }
+
+  sendVersions({ msg, send, done }) {
+    msg.payload = this.versions
+    send(msg)
+    done()
+  }
+}
+
+module.exports = { GameOnVersionInfo }
--- gameon-mcp23017.js
+++ gameon-mcp23017.js
@@ -1,25 +1,36 @@
 const { MCP23017Chip } = require('./MCP23017ChipManager')
 const { OutputPinManager } = require('./OutputPinManager')
 const { InputPinManager } = require('./InputPinManager')
+const { GameOnVersionInfo } = require('./GameOnVersionInfo')
 
 module.exports = (RED) => {
 
   function MCP23017ChipConfigNode(config) {
-    RED.nodes.createNode(this, config)
-    this.chip = new MCP23017Chip({ RED, node: this, ...config })
+    const node = this
+    RED.nodes.createNode(node, config)
+    this.chip = new MCP23017Chip({ RED, node, ...config })
   }
   RED.nodes.registerType('gameon-mcp23017-chip', MCP23017ChipConfigNode)
 
   function MCP23017OutputPinNode(config) {
-    RED.nodes.createNode(this, config)
-    this.eventManager = new OutputPinManager({ RED, node: this, ...config })
+    const node = this
+    RED.nodes.createNode(node, config)
+    this.eventManager = new OutputPinManager({ RED, node, ...config })
   }
   RED.nodes.registerType('gameon-mcp23017-output', MCP23017OutputPinNode)
 
   function MCP23017InputPinNode(config) {
-    RED.nodes.createNode(this, config)
-    this.eventManager = new InputPinManager({ RED, node: this, ...config })
+    const node = this
+    RED.nodes.createNode(node, config)
+    this.eventManager = new InputPinManager({ RED, node, ...config })
   }
   RED.nodes.registerType('gameon-mcp23017-input', MCP23017InputPinNode)
+
+  function GameOnMCP23017VersionNode(config) {
+    const node = this
+    RED.nodes.createNode(node, config)
+    this.version = new GameOnVersionInfo({node})
+  }
+  RED.nodes.registerType('gameon-mcp23017-version', GameOnMCP23017VersionNode)
 
 }
